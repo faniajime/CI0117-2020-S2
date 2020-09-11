f862ac86231999c0458753b8132963597c14da68
Fabiola Jimenez <faniajime@gmail.com>
2020-09-10 19:37:57 -0600

    Added mutexes

---
+++ laboratorios/3/hello_ordered_mutex/README.md
@@ -0,0 +1,4 @@
+ 
+### ¿Permite este mecanismo de control de concurrencia resolver este problema de manera predecible?
+
+Si, utilizando un mutex sabemos cual hilo esta ejecutando una poricon de codigo en un tiempo determinado. Es decir, si es predecible la seccion controlada por le mutex.
--- laboratorios/3/hello_ordered_mutex/hello_ordered_mutex.c
+++ laboratorios/3/hello_ordered_mutex/hello_ordered_mutex.c
@@ -1,16 +1,18 @@
- 
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 typedef struct {
-    size_t next_thread;
-} shared_message_t;
+    size_t position;
+    size_t num_threads;
+    pthread_mutex_t* mutexes;
+
+} shared_data_t;
 
 typedef struct {
+    
     size_t thread_num;
-    shared_message_t* shared_message;
+    shared_data_t* shared_data;
 
 } thread_data_t;
 
@@ -20,59 +22,66 @@ void* helloWorld(void* args) {
     thread_data_t* data = (thread_data_t*) args;
 
     size_t thread_num = data->thread_num;
-    shared_message_t* shared_message = data->shared_message;
+    shared_data_t* shared_data = data->shared_data;
+    size_t num_threads = shared_data->num_threads;
 
-    while (thread_num != shared_message->next_thread);
 
-    printf("Hello world from thread number # %zu.\n", thread_num);
-    
-    ++shared_message->next_thread;
-    
+    if(thread_num == 0){
+        for (size_t i = 1; i < num_threads; ++i) {
+            pthread_mutex_lock(&shared_data->mutexes[i]);
+        }
+    }
+    printf("Im the thread %zu and I arrived %zu\n", thread_num, shared_data->position);
+    ++shared_data->position;
+    pthread_mutex_unlock(&shared_data->mutexes[shared_data->position]);
+      
     return NULL;
 }
 
-
 int main(int argc, char* arg[]) {
 
-    struct timespec inicio, final; 
-    int dif, menor;
-    menor = 100000;
-
-
-    clock_gettime(CLOCK_REALTIME, &inicio);
     size_t thread_count = 0;
 
     if (argc >= 2) {
         thread_count = (size_t)strtoul(arg[1], NULL, 10);
+
     } else {
         fprintf(stderr, "Error, invalid number of parameters\n");
         return 1;
     }
 
     pthread_t* threads = malloc((size_t)(thread_count * sizeof(pthread_t)));
+    shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
 
-    shared_message_t* shared_message = (shared_message_t*)calloc(1, sizeof(shared_message_t));
-    shared_message->next_thread = 0;
+    shared_data->mutexes = (pthread_mutex_t*)calloc(thread_count, sizeof(pthread_mutex_t));
 
+    shared_data->num_threads = thread_count;
+    shared_data->position = 0;
+
+    for (size_t i = 0; i < thread_count; ++i) {
+        pthread_mutex_init(&shared_data->mutexes[i], /*attr*/ NULL);
+    }
+    
     thread_data_t* thread_data_list = malloc((size_t)(thread_count * sizeof(thread_data_t)));
 
     for (size_t i = 0; i < thread_count; ++i) {
         thread_data_list[i].thread_num = i;
-        thread_data_list[i].shared_message = shared_message;
+        thread_data_list[i].shared_data = shared_data;
         pthread_create(&threads[i], NULL, helloWorld, (void*)&thread_data_list[i]);
     }
-
 
     for (size_t i = 0; i < thread_count; ++i) {
         pthread_join(threads[i], NULL);
     }
-    clock_gettime(CLOCK_REALTIME, &final);
-    dif = (final.tv_nsec - inicio.tv_nsec);
-    printf("Hello from the main thread, this solution took %u nanoseconds to run.\n",dif);
 
+    for (size_t i = 0; i < thread_count; ++i) {
+        pthread_mutex_destroy(&shared_data->mutexes[i]);
+    }
+    
     free(threads);
-    free(shared_message);
+    free(shared_data->mutexes);
+    free(shared_data);
     free(thread_data_list);
 
     return 0;
-} 
+}
