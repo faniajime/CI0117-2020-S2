5c4acc7cd9b0b238a028cd9f55c4f7c9a5a59363
Fabiola Jiménez-González <45375479+faniajime@users.noreply.github.com>
2020-10-09 12:04:25 -0600

    Add files via upload

---
+++ dna_sequences/dna_sequences.h
@@ -0,0 +1,8 @@
+#ifndef DNA_SEQUENCES_H
+#define DNA_SEQUENCES_H
+
+const char sequence1[] = "asjdkqoijwrmfpqefqweqweqwe";
+const char sequence2[] = "vierespequenasjaskdjoeiwnmqpwmdpqowekqwneoqwirnwrpokqweoqwieqoweiqowei";
+const int len1 = 26;
+const int len2 = 70;
+#endif
