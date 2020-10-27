21135cb997cedb948caef33a253fef83fa88c3b6
Fabiola <faniajime@gmail.com>
2020-10-27 08:09:23 -0600

    fucking stoi

--- Actividades/Actividad_5/average_parallel/average_parallel.cpp
+++ Actividades/Actividad_5/average_parallel/average_parallel.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-static void calculate_sum(char **array, int array_size, double* global_sum){
+int calculate_sum(char **array, int array_size, double global_sum){
 
     int num_thread = omp_get_thread_num();
     double sum = 0;
@@ -14,13 +14,14 @@ static void calculate_sum(char **array, int array_size, double* global_sum){
     for( int i=0; i<array_size; i++)
     {
         printf("Thread %d: Processing number %s", num_thread, array[num_thread+i]);
-        sum = sum + stoi(array[num_thread+i]);
+        sum = sum + stoi(array[1+num_thread+i]);
     }
     
     #pragma omp critical
     {
-        global_sum[0] = global_sum[0] + sum;
+        global_sum+= sum;
     }
+    return global_sum;
 }
 
 int main(int argc, char *argv[])
@@ -37,10 +38,10 @@ int main(int argc, char *argv[])
     thread_nums = global_size;
 
 
-    #pragma omp parallel num_threads(thread_nums)
+    #pragma omp parallel num_threads(thread_nums) shared(global_sum)
     {
         int num_thread = omp_get_thread_num();
-        calculate_sum(&argv[1+num_thread], global_size, &global_sum );
+        calculate_sum(&argv[1+num_thread], global_size, global_sum);
     }            
 
     cout << "Average: " << (global_sum / global_size) << endl;
