75930666d227daf46196ab80dd8d1338ca03be0c
Fabiola <faniajime@gmail.com>
2020-11-10 08:54:20 -0600

    Nambre

--- Actividades/Actividad_7/hello_mpi_ordered/hello_mpi_ordered.cpp
+++ Actividades/Actividad_7/hello_mpi_ordered/hello_mpi_ordered.cpp
@@ -12,15 +12,16 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     int message_sent = 0;
     int message_recieved = 0;
+    int first = 0;
     
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
     if(my_id ==0 ){
-        for (int i = 1; i<num_processes; i++){
-            message_sent = i;
-            MPI_Send(&message_sent, 1 /*count*/, MPI_INT, i /*dest*/, i /*message id*/, MPI_COMM_WORLD);
-            sleep(1);
-        }
+        message_sent = 1;
+        MPI_Send(&message_sent, 1 /*count*/, MPI_INT, 1 /*dest*/, 1 /*message id*/, MPI_COMM_WORLD);
+        MPI_Send(&message_sent, 1 /*count*/, MPI_INT, 2 /*dest*/, 2 /*message id*/, MPI_COMM_WORLD);
+        MPI_Send(&message_sent, 1 /*count*/, MPI_INT, 3 /*dest*/, 3 /*message id*/, MPI_COMM_WORLD);
+
     }else{
         MPI_Recv(&message_recieved, 1 /* count*/, MPI_INT, 0 /*source*/, my_id /*message id*/, MPI_COMM_WORLD, &status);
     }
