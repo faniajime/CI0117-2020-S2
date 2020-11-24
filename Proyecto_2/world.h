6d0badc2a52ccf28639ac9a441ac2edfd887efca
Fabiola <faniajime@gmail.com>
2020-11-23 22:25:42 -0600

    xd

--- Proyecto_2/world.h
+++ Proyecto_2/world.h
@@ -1,1 +1,59 @@
- 
+ #include <iostream>
+ #include <string>
+
+ using namespace std;
+
+class World_Element
+{
+    private:
+        void get_actions(){};
+};
+
+class Coin: public World_Element
+{
+    const string actions[2] = {"NO_JUMP", "JUMP"};
+    void get_actions(){};
+    double get_action_probability(string action){};
+
+};
+
+class Hole: public World_Element
+{
+    const string actions[2] = {"NO_JUMP", "JUMP"};
+    void get_actions(){};
+    double get_action_probability(string action){};
+
+};
+
+class littleGoomba: public World_Element
+{
+    const string actions[3] = {"NO_JUMP", "JUMP_PASS", "JUMP_ATTACK"};
+    void get_actions(){};
+    double get_action_probability(string action){};
+    void kill_enemy(){};
+
+};
+
+class koopaTroopa: public World_Element
+{
+    const string actions[3] = {"NO_JUMP", "JUMP_PASS", "JUMP_ATTACK"};
+    void get_actions(){};
+    double get_action_probability(string action){};
+    void kill_enemy(){};
+    
+};
+
+ class World
+ {    
+    void initialize(){};
+    void destroy_world(){};
+    public:
+        World_Element* my_world;
+        void getNext();
+    private:
+        int current_position;
+        void updateWorld(){};
+
+        
+
+ };
