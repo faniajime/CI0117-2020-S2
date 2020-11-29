2bb4432f83583f319a59d9d6a78ae50f22d65362
Fabiola <faniajime@gmail.com>
2020-11-29 13:57:30 -0600

    kk

--- Proyecto_2/world.cpp
+++ Proyecto_2/world.cpp
@@ -4,6 +4,34 @@
 
 using namespace std;
 
-string* World_Element::get_actions(){
-    return actions;
-}
+World::World(World world, vector<vector<int>> baseWorld){
+    world.myWorld = baseWorld;
+    world.world_size = baseWorld.size();
+};
+
+World::~World(){
+    free(world->myWorld);
+};
+
+vector<int> World::getNext(World world){
+    world.current_position+=1;
+    return world.myWorld[world.current_position];
+};
+
+void World::refreshWorld(World world, int koopatroopas){
+    if(koopatroopas){
+        world.myWorld = baseWorld;
+        world.addElement(koopatroopas);
+    }else{
+        world.myWorld = baseWorld;
+    }
+};
+void World::addElement(World world, int koopatroopas){
+    if( world.world_size - world.current_position < 10){
+        pass;
+    }else{
+        for( int i =0; i<koopatroopas; i++){
+            world.myWorld[world.current_position+10].push_back(2);
+        }
+    }
+};
--- Proyecto_2/world.h
+++ Proyecto_2/world.h
@@ -13,55 +13,21 @@ enum Elements
     Hole = 1,
     Goomba = 2,
     KoopaTroopa = 3,
-    Coin = 4,
+    Coin = 4
 };
 
 
-class World_Element
-{
-    public:
-        string* get_actions(){};
-    private:
-        string* actions;
-};
-
-class Coin: public World_Element
-{
-    const string actions[2] = {"NO_JUMP", "JUMP"};
-    void get_action(){};
-
-};
-
-class Hole: public World_Element
-{
-    const string actions[2] = {"NO_JUMP", "JUMP"};
-    void get_actions(){};
-
-};
-
-class littleGoomba: public World_Element
-{
-    const string actions[3] = {"NO_JUMP", "JUMP_PASS", "JUMP_ATTACK"};
-    void get_actions(){};
-
-};
-
-class koopaTroopa: public World_Element
-{
-    const string actions[3] = {"NO_JUMP", "JUMP_PASS", "JUMP_ATTACK"};
-    void get_actions(){};
+class World
+{  
+World(vector<vector<int>> baseWorld){};
+~World(){};
+public:
     
+    vector<int> getNext();
+private:
+    vector<vector<int>> myWorld;
+    int world_size;
+    int current_position;
+    void refreshWorld(){};
+    void addElement(){};
 };
-
- class World
- {  
-    World(vector<vector<int>> baseWorld){};
-    ~Mario(){};
-    public:
-        World_Element* my_world;
-        void getNext();
-    private:
-        int current_position;
-        void refreshWorld(){};
-        void addElement(){};
- };
